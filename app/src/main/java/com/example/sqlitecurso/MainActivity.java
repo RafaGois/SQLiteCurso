62c0a5550c062b657e37151788cbebea2fc6680d
Rafael Fontana Becker Gois <87453597+RafaGois@users.noreply.github.com>
2022-10-03 21:59:01 -0300

    criacao de activity tab navigation

--- app/src/main/java/com/example/sqlitecurso/MainActivity.java
+++ app/src/main/java/com/example/sqlitecurso/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqlitecurso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -16,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void navegaAddActivity (View view){
+        Intent intent = new Intent(this,Adicao.class);
+        startActivity(intent);
+    }
+
+    public void navegaGraficosActivity (View view){
+        Intent intent = new Intent(this,Graficos.class);
+        startActivity(intent);
     }
 }
